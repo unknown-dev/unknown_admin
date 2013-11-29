cf4551419a43e0917801b6d2a38985ecdb3f5ea1
Stefan Kahlert <kahlert@unknown.de>
2013-11-29 14:51:20 +0100

    fixed link to add fields method?

--- app/helpers/admin/resources_helper.rb
+++ app/helpers/admin/resources_helper.rb
@@ -26,13 +26,13 @@ module Admin
       end
     end
 
-    def link_to_add_fields(name, f, type)
-      new_object = f.object.send "build_#{type}"
-      id = "new_#{type}"
-      fields = f.send("#{type}_fields", new_object, child_index: id) do |builder|
-        render(type.to_s + "_fields", f: builder)
+    def link_to_add_fields(name, f, association)
+      new_object = f.object.send(association).klass.new
+      id = new_object.object_id
+      fields = f.fields_for(association, new_object, child_index: id) do |builder|
+        render(association.to_s.singularize + "_fields", f: builder)
       end
-      link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+      link_to(name, '#', class: "btn btn-primary add_fields", data: {id: id, fields: fields.gsub("\n", "")})
     end
 
 
