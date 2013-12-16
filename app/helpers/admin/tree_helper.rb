eb41fde7c1d3d546b1161f5e3390d82d2b9b71d4
Stefan Kahlert <kahlert@unknown.de>
2013-12-16 14:33:22 +0100

    Fixed font-awesome and button classes

--- app/helpers/admin/tree_helper.rb
+++ app/helpers/admin/tree_helper.rb
@@ -3,7 +3,7 @@ module Admin
 		def nested_nodes(nodes)
       nodes.map do |node, sub_nodes|
         content_tag(:div,:class=>"nested_nodes media")  do 
-          nested = content_tag(:i,"",:class=>"pull-left icon-folder-open")
+          nested = content_tag(:i,"",:class=>"pull-left fa fa-folder-open")
           nested << content_tag(:div, :class=>"media-body")  do
             render(partial:"tree_node",locals:{node:node})+nested_nodes(sub_nodes)
           end
