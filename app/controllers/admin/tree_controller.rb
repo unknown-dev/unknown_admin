93bb7f02c4eaee05c59b5b734950e189bc20a781
Stefan Kahlert <kahlert@unknown.de>
2014-01-15 15:45:06 +0100

    Added sorting custom actions

--- app/controllers/admin/tree_controller.rb
+++ app/controllers/admin/tree_controller.rb
@@ -1,5 +1,13 @@
 module Admin
 	class TreeController < ResourcesController
+		custom_actions :resource => [:move_higher,:move_lower]
+		def move_higher
+      move_higher!{resource.move_higher}
+    end
+
+    def move_lower
+      move_lower!{resource.move_lower}
+    end
 
 		respond_to :html,:js
 		protected
